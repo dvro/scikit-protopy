66b33d9d8e1e5a25a960e249da5a5cdcff7d9195
Dayvid Victor <victor.dvro@gmail.com>
2014-06-29 21:05:23 -0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='protopy',
+    version='0.1',
+    description='python package for Prototype Selection and Generation',
+    author='Dayvid Victor',
+    author_email='victor.dvro@gmail.com',
+    url='https://github.com/dvro/scikit-protopy',
+    packages=['protopy'],
+    license='BSD License',
+    install_requires=[
+        'scikit-learn',
+        'numpy'
+    ],
+    platforms='Any',)
