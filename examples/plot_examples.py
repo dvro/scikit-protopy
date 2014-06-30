236e003a78421c294f3087e8f09287a56d9c34db
Dayvid Victor <victor.dvro@gmail.com>
2014-06-30 16:40:01 -0300

    plot_examples.py updated (shuffle)

--- examples/plot_examples.py
+++ examples/plot_examples.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import random
+
 from protopy.selection.enn import ENN
 from protopy.selection.cnn import CNN
 from protopy.selection.renn import RENN
@@ -14,7 +16,7 @@ f, subfig = plt.subplots(3,2)
 mu1 = [4, 5]
 si1 = [[0.75, 0.25], [0.25, 0.75]]
 
-mu2 = [5, 5]
+mu2 = [6, 5]
 si2 = [[0.25, 0.75], [0.75, 0.25]]
 
 samples = 100
@@ -25,6 +27,13 @@ X2 = np.random.multivariate_normal(
     np.asarray(mu2), np.asarray(si2), samples)
 X = np.vstack((X1, X2))
 y = np.asarray([0] * samples + [1] * samples)
+
+z = zip(X, y)
+random.shuffle(z)
+X, y = zip(*z)
+X, y = np.asarray(X), np.asarray(y)
+
+
 
 
 algorithms = [ENN(), RENN(), AllKNN(), TomekLinks(), CNN()]
@@ -39,6 +48,7 @@ for i in range(3):
             subfig[i][j].set_title('Original Dataset')
         elif index < len(algorithms):
             X_, y_ = algorithms[index].reduce_data(X, y)
+            print algorithms[index], 'reduction: %.2f' % (algorithms[index].reduction_)
             subfig[i][j].plot(X_[y_==0].T[0], X_[y_==0].T[1], 'bs', X_[y_==1].T[0], X_[y_==1].T[1],'ro')
             subfig[i][j].axis([0, 10, 0, 10])
             subfig[i][j].set_title(titles[index])
